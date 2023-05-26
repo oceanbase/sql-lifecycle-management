f8e5c3ab58654bb2dc12274a093d0fb7a87262fe
Ifffff <ifztk329@gmail.com>
2023-05-26 14:52:17 +0800

    dependencies: add chardet

--- setup.py
+++ setup.py
@@ -44,7 +44,8 @@ setup(
         'simplejson==3.18.4',
         'schedule',
         'flasgger',
-        'ply==3.11'
+        'ply==3.11',
+        'chardet==3.0.4'
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
