bff779f8f595d5026763e8bb7419c0e18fefe80d
Arthur Chern <weirong.cwr@gmail.com>
2023-06-26 19:23:17 +0800

    feat: init agent module

---
+++ agent/toolset/toolset.go
@@ -0,0 +1,10 @@
+package toolset
+
+import (
+	_ "github.com/AlekSi/gocov-xml"
+	_ "github.com/axw/gocov/gocov"
+	_ "github.com/mgechev/revive"
+	_ "github.com/segmentio/golines"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "gotest.tools/gotestsum"
+)
